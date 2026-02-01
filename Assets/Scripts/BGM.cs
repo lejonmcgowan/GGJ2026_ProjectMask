b6c3a46139bc0d73240f5840971b6a889877b697
LeJon McGowan <lejonmcgowan@gmail.com>
2026-02-01 13:58:36 -0800

    fix duplicate BGM issue

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -1,11 +1,22 @@
+using System;
 using UnityEngine;
 
 public class BGM : MonoBehaviour
 {
+    public AudioSource AudioSource; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (FindObjectsByType<BGM>(FindObjectsSortMode.None).Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+            
     }
 
     // Update is called once per frame
